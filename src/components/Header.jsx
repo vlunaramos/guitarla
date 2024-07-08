9f9a37a9a5e4afa8d9e7805560851d7f768fa3fe
Victor Luna <victor.luna@ieebc.com>
2024-07-08 13:59:18 -0700

    add total

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
 function Header({ cart }) {
+    // state derivado
+    const isEmpty = () => cart.length === 0
+    const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0)
+
     return (
         <header className="py-5 header">
+           
             <div className="container-xl">
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
@@ -15,8 +20,8 @@ function Header({ cart }) {
                             <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
-                                {cart.lengh === 0 ? (<p className="text-center">El carrito esta vacio</p>) :
-                                    (
+                                {isEmpty() ? (<p className="text-center">El carrito esta vacio</p>) :
+                                    (<>
                                         <table className="w-100 table">
                                             <thead>
                                                 <tr>
@@ -64,8 +69,10 @@ function Header({ cart }) {
                                                 ))}
                                             </tbody>
                                         </table>
+
+                                        <p className="text-end">Total pagar: $ {cartTotal()}<span className="fw-bold"></span></p>
+                                    </>
                                     )}
-                                <p className="text-end">Total pagar: <span className="fw-bold"></span></p>
                                 <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                             </div>
                         </div>
