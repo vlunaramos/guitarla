0538e26131f9cbdff75d5c8a52d562dd7b107b99
Victor Luna <victor.luna@ieebc.com>
2024-07-11 13:37:53 -0700

    rmove assets bad url

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     return localStorageCart ? JSON.parse(localStorageCart) : []
   }
 
-  const [data, setData] = useState(db)
+  const [data] = useState(db)
   const [cart, setCart] = useState(initialCart)
 
   useEffect(() => {
